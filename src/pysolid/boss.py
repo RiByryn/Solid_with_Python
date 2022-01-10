b4a6f11ce2ae53c3272fecc421abcdd9f884298c
RiByryn <maryantonovichru@gmail.com>
2022-01-10 11:13:41 +0300

    Добавлен комментарий

--- src/pysolid/boss.py
+++ src/pysolid/boss.py
@@ -25,22 +25,22 @@ class Boss():
 
             
             self.part.FeatureManager.FeatureExtrusion2(
-                True, # SD: True for single ended, false for double ended
+                True,                # SD: True for single ended, false for double ended
+                False,               #Flip: True to flip side to cut
+                False,               #Dir: True to flip the direction to extrude
+                0,                   #T1: Termination type for first end as defined in swEndConditions_e
+                0,                   #T2: Termination type for second end as defined in swEndConditions_e
+                self.height*MM_TO_M, #D1: Depth of extrusion for first end in meters
+                self.height*MM_TO_M, #D2: Depth of extrusion for second end in meters
+                False,               #Dchk1: True allows draft angle in first direction, false does not allow drafting
+                False,               #Dchk2: True allows draft angle in second direction, false does not allow drafting
+                False,               #Ddir1: True for first draft angle to be inward, false to be outward
+                False,               #Ddir2: True for second draft angle to be inward, false to be outward
+                0,                   #Dang1: Draft angle for first end
+                0,                   #Dang2: Draft angle for second end
+                False,               #OffsetReverse1: If you chose to offset the first end condition from another face or plane, then True specifies offset in direction away from the sketch, false specifies offset from the face or plane in direction toward the sketch
+                False,               #OffsetReverse2: If you chose to offset the second end condition from another face or plane, then True specifies offset in direction away from the sketch, false specifies offset from the face or plane in direction toward the sketch
                 False, 
-                False, 
-                0, 
-                0,
-                self.height*MM_TO_M,
-                self.height*MM_TO_M,
-                False, 
-                False,
-                False,
-                False,
-                0,
-                0,
-                False,
-                False,
-                False,
                 False,
                 True,
                 True,
